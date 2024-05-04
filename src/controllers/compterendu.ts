a0e33485139407803e47a458aa361bf472c91383
ChedlyRebai <chedly.rebai123@gmail.com>
2024-05-04 15:19:20 +0100

    refactor: Update clientInjoignable model and related controllers

--- src/controllers/compterendu.ts
+++ src/controllers/compterendu.ts
@@ -82,7 +82,145 @@ export const getHistoriqueCompteRendu = async (req: Request, res: Response) => {
       .json({ message: "Internal Server Error" });
   }
 };
+
 export const createCompteRendu = async (req: Request, res: Response) => {
+  try {
+    const { type,user, mnt_reg,observation,suiviAgenda, compteRendu, cli, clientInjoignableId } = req.body;
+    
+    // Fetching the number of existing suivi_agenda entries for the specified client
+    // const num = await db.suivi_agenda.count({
+    //   where: {
+    //     cli: cli,
+    //   },
+    // });
+
+    // // Creating the new compterendu entry
+    // const newCompteRendu = await db.suivi_agenda.create({
+    //   data: {
+    //     //Assuming id is autoincremented and unique, you don't need to specify it
+    //     num: num + 1,// Incrementing num
+    //     cli: cli,
+    //     date_ag: new Date(), // Assuming you want the current date
+    //     compte_rendu: compteRendu,
+    //     usr_nom: user.role,
+    //     usr_matricule: user.matricule,
+        
+    //   },
+    // });
+    const conmpterendutypes = await db.types.findMany();
+      
+    const nouvelleCompteRendu = await db.suivi_agenda.create({
+      data: {
+        id: 10,
+        num: 1,
+        cli: cli,
+        date_ag: new Date(),
+        compte_rendu: compteRendu,
+        usr_nom: user.role,
+        usr_matricule: user.matricule,
+        comptretypeid:2
+      },
+    });
+    if(type==1){
+      const promesse = await db.promesseregresse.create({
+        data: {
+          mnt_reg: mnt_reg,
+          lieu_ver: req.body.lieu_ver,
+          date_ver: req.body.date_ver,
+          suiviagendaid:2
+        },
+      });
+
+      const compterendutype=await db.compterendutype.create({
+        data:{
+          compterenduid:nouvelleCompteRendu.id,
+          nouvellecoordonneesID:promesse.id,
+          typeID:1
+        }
+      })
+
+      return res.status(StatusCodes.OK).json(promesse);
+
+    }
+    console.log(type)
+    if(type==2){
+      const novellecoordonnee = await db.nouvellecoordonnees.create({
+        data: {
+          nouv_te2: req.body.nouv_te2,
+          nouv_tel: req.body.nouv_tel,
+          nouv_adresse: req.body.nouv_adresse,
+          suiviagendaid:1
+        },
+      });
+      const compterendutype=await db.compterendutype.create({
+        data:{
+          compterenduid:nouvelleCompteRendu.id,
+          nouvellecoordonneesID:novellecoordonnee.id,
+          typeID:2
+        }
+      })
+
+      return res.status(StatusCodes.OK).json(novellecoordonnee);
+    }
+
+    if(type==3){
+      const facilitePaiment = await db.facilitePaiment.create({
+        data: {
+
+        },
+      });
+    }
+
+    if(type==4){
+      const nonreconaissance = await db.nonreconaissance.create({
+        data: {
+          observation:observation
+        },
+      });
+      const compterendutype=await db.compterendutype.create({
+        data:{
+          compterenduid:nouvelleCompteRendu.id,
+          nouvellecoordonneesID:nonreconaissance.id,
+          typeID:4
+        }
+      })
+    }
+
+    if(type==5){
+      const visite = await db.visite.create({
+        data: {
+          date_visite: req.body.date_visite,
+          h_rdv: req.body.h_rdv,
+          lieu_visite: req.body.lieu_visite,
+
+        },
+      });
+      const compterendutype=await db.compterendutype.create({
+        data:{
+          compterenduid:nouvelleCompteRendu.id,
+          nouvellecoordonneesID:visite.id,
+          typeID:5
+        }
+      })
+    }
+
+    if(type==6){
+      const clientInjoignable = await db.clientInjoignable.create({
+        data: {
+          
+        },
+      });
+
+    }
+
+    console.log(req.body);
+    return res.status(StatusCodes.OK).json(req.body);
+  } catch (error) {
+    console.error(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Internal Server Error" });
+  }
+};
+export const createCompteRend = async (req: Request, res: Response) => {
   try {
     const { user, suiviAgenda, compteRendu, cli, clientInjoignableId } = req.body;
 
@@ -96,8 +234,8 @@ export const createCompteRendu = async (req: Request, res: Response) => {
     // Creating the new compterendu entry
     const newCompteRendu = await db.suivi_agenda.create({
       data: {
-        // Assuming id is autoincremented and unique, you don't need to specify it
-        num: num + 1, // Incrementing num
+        //Assuming id is autoincremented and unique, you don't need to specify it
+        num: num + 1,// Incrementing num
         cli: cli,
         date_ag: new Date(), // Assuming you want the current date
         compte_rendu: compteRendu,
