3129b85fad28fd6f4ad674ba40f3277f2d097f68
ChedlyRebai <chedly.rebai123@gmail.com>
2024-05-17 10:33:32 +0100

    refactor: Update cron script to execute generateAlerts.ts instead of generateAlerts.js

--- src/controllers/cron.ts
+++ src/controllers/cron.ts
@@ -3,7 +3,7 @@ import cron from "node-cron";
 // Schedule the script to run every day at midnight
 cron.schedule("0 0 * * *", () => {
   console.log("Running generateAlerts.js script...");
-  exec("node path/to/generateAlerts.js", (error, stdout, stderr) => {
+  exec("../controllers/generateAlerts.ts", (error, stdout, stderr) => {
     if (error) {
       console.error(`Error executing script: ${error.message}`);
       return;
