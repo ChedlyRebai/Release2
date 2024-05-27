82c86b7efac0b3ee8ce92df3a781acb314a4ab4f
ChedlyRebai <chedly.rebai123@gmail.com>
2024-05-27 19:31:00 +0100

    refactor: Remove commented out code and console.log statements in cron.ts

--- src/controllers/cron.ts
+++ src/controllers/cron.ts
@@ -1,24 +1,4 @@
 import { exec } from "child_process";
-//import cron from "node-cron";
-// // Schedule the script to run every day at midnight
-// cron.schedule("0 0 * * *", () => {
-//   console.log("Running generateAlerts.js script...");
-//   exec("../controllers/generateAlerts.ts", (error, stdout, stderr) => {
-//     if (error) {
-//       console.error(`Error executing script: ${error.message}`);
-//       return;
-//     }
-
-//     if (stderr) {
-//       console.error(`Script stderr: ${stderr}`);
-//       return;
-//     }
-
-//     console.log(`Script stdout: ${stdout}`);
-//   });
-// });
-
-// /cron/cron.ts
 
 import cron from "node-cron";
 import generateAlerts from "../controllers/generateAlerts";
