c69952482e2a19582b6847506bf84d8983cd6a8f
ChedlyRebai <chedly.rebai123@gmail.com>
2024-05-28 15:16:48 +0100

    refactor: Update lettre controller routes to use consistent formatting

--- src/routes/lettrerecouvrement.ts
+++ src/routes/lettrerecouvrement.ts
@@ -3,10 +3,7 @@ import { getLettres, toggleEtatLettre } from "../controllers/lettre";
 
 const router: Router = express.Router();
 
-
-router.get("/getlettre",getLettres)
-router.put("/updatelettre",toggleEtatLettre)
+router.get("/getlettre", getLettres);
+router.put("/updatelettre", toggleEtatLettre);
 
 export default router;
-
-
